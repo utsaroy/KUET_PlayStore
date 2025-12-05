fc5c058afc71fd036615004a2a8b4b57547e06c9
Utsa Roy <utsaroy3@gmail.com>
2025-12-05 11:33:59 +0600

    Created Projects

---
+++ playstore_desktop/src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module com.utsa.kpstore.playstore_desktop {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens com.utsa.kpstore.playstore_desktop to javafx.fxml;
+    exports com.utsa.kpstore.playstore_desktop;
+}
